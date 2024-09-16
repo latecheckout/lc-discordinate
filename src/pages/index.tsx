87a216eb8f3fc5452e2f5ccff1ab4ed5f8321c65
Tim Everett <tim.neil.everett@gmail.com>
2024-09-16 12:05:07 -0300

    feat: add auth guard to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,24 @@
+import { useAuth } from '@/contexts/auth.context'
+import { useRouter } from 'next/router'
+import { use, useEffect } from 'react'
+
 export default function Home() {
-  return <main>HOME</main>
+  const { user, status } = useAuth()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (status === 'unauthenticated') {
+      router.push('/sign-in')
+    }
+  }, [status, router])
+
+  if (status === 'loading') {
+    return <div>Loading...</div>
+  }
+
+  if (status === 'unauthenticated') {
+    return null
+  }
+
+  return <div>Welcome, {user?.email}</div>
 }
